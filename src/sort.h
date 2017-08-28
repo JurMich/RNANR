e2d6fa29f22e4117ea8f452e3761912aeda24cbf
¨Juraj <juraj.michalik.2048@gmail.com>
2017-08-28 16:04:12 +0200

    Updated license.

--- src/sort.h
+++ src/sort.h
@@ -1,3 +1,18 @@
+/*
+* This file constitutes a part of RNANR program.
+*
+* RNANR is free software to which the terms of the GNU Affero General 
+* Public License apply as published by the Free Software Foundation, 
+* either of version 3, or (at your option) any later version.
+*
+* This program is distributed without ANY WARRANTY or RESPONSIBILITY.
+* You can distribute and change its contents, youŕe not allowed to SELL it.
+* For more precisions, see the GNU Affero General Public License.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef SORT_H_
 #define SORT_H_
 
